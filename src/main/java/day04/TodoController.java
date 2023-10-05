8c4f18be887982aa80511be6120b762d0f216086
kkchanss <rlacksgml99@naver.com>
2023-10-05 14:07:50 +0900

    todo Spring 정적파일 반환하는 방법

--- src/main/java/day04/TodoController.java
+++ src/main/java/day04/TodoController.java
@@ -2,7 +2,9 @@ package day04;
 
 // 스프링 빈 : 스프링 컨테이너(저장소)에 저장된 객체 [ 이유 : 스프링이 대신 객체 관리 => 여러 개발자들이 작업했을때 기준 ]
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,7 +12,11 @@ import java.util.List;
 //@Controller // Spring MVC 중 해당 클래스를 Controller로 사용 / 스프링 컨테이너 빈 등록
 @RestController // Controller + ResponseBody
 @RequestMapping("/todo") // HTTP로부터의 해당 클래스의 매핑 주소 만들기 // 공통 URL
-public class TodoControllr {
+public class TodoController {
+
+    @Autowired // 미리 등록한 스프링 컨테이너에서 빈 찾아서 주입
+    private TodoService todoService;
+
     // REST : HTTP기반으로 GET, POST, PUT, DELETE 메소드 이용한 웹 서비스
     // 1. [C]
     @PostMapping("") // HTTP 요청중 POST 메소드 요청할때
@@ -18,14 +24,14 @@ public class TodoControllr {
     public boolean doPost(@RequestBody TodoDto todoDto) { // 요청 매개변수 : 입력받은 정보들 [ Dto }
         System.out.println("TodoControllr.doPost");
         System.out.println("todoDto = " + todoDto);
-        return false;
+        return todoService.doPost(todoDto);
     }
 
     // 2. [R]
     @GetMapping("") //@ResponseBody
     public List<TodoDto> doGet() {
         System.out.println("TodoControllr.doGet");
-        return false;
+        return todoService.doGet();
     }
 
     // 3. [U]
@@ -33,7 +39,7 @@ public class TodoControllr {
     public boolean doPut(@RequestBody  TodoDto todoDto) {
         System.out.println("TodoControllr.doPut");
         System.out.println("todoDto = " + todoDto);
-        return false;
+        return todoService.doPut(todoDto);
     }
 
     // 4. [D]
@@ -41,6 +47,14 @@ public class TodoControllr {
     public boolean doDelete(@RequestParam int tno) {
         System.out.println("TodoControllr.doDelete");
         System.out.println("tno = " + tno);
-        return false;
+        return todoService.doDelete(tno);
     }
+
+    // 5.
+    @GetMapping("/index")
+    public Resource getIndex(){
+
+        return new ClassPathResource("templates/todo.html");
+    }
+
 }
--- src/main/java/day04/TodoDto.java
+++ src/main/java/day04/TodoDto.java
@@ -9,5 +9,5 @@ import lombok.*;
 public class TodoDto {
     private int tno;
     private String tcontent;
-    private String tstate;
+    private boolean tstate;
 }
